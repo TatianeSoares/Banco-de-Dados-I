6a63cc27f4b7c734a7fad712d5e3c532c21ff88e
Tatiane <soaresferreiratatiane@gmail.com>
2022-11-06 00:42:13 -0300

    Arrumando dependencias do projeto

---
+++ project/src/main/java/controller/inputController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class inputController implements Serializable {
+
+  public void init(){
+
+  }
+
+  public void verificarArquivo(){
+
+  }
+}

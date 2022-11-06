4b8bb6ee71f9f37a7a50fdee81eecab90f29b8c3
Kauee Rocha Puertas <kauee-rocha@hotmail.com>
2022-11-06 15:58:00 -0300

    CRUD DAO's finalizados sem as FK

--- project/src/main/java/DAO/Sinalizacao/PgVelocidadeMaximaDAO.java
+++ project/src/main/java/DAO/Sinalizacao/PgVelocidadeMaximaDAO.java
@@ -3,10 +3,16 @@ package DAO.Sinalizacao;
 
 import model.Ultrapassagem;
 import model.VelocidadeMaxima;
+import org.omnifaces.util.Messages;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PgVelocidadeMaximaDAO implements VelocidadeMaximaDAO{
 
@@ -63,26 +69,106 @@ public class PgVelocidadeMaximaDAO implements VelocidadeMaximaDAO{
 
     @Override
     public void create(VelocidadeMaxima velocidadeMaxima) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_VELOCIDADE_MAXIMA)) {
+            statement.setString(1, velocidadeMaxima.getSituacao());
+            statement.setString(2, velocidadeMaxima.getUf());
+            statement.setInt(3, velocidadeMaxima.getAnoPnvSnc());
+            statement.setFloat(4, velocidadeMaxima.getVeloVeicPesado());
+            statement.setFloat(5, velocidadeMaxima.getVeloVeicLeve());
+            statement.setFloat(6, velocidadeMaxima.getLatitude());
+            statement.setFloat(7, velocidadeMaxima.getLongitude());
+            statement.setString(8, velocidadeMaxima.getMunicipio());
+            statement.setFloat(9, velocidadeMaxima.getKm());
+            //TODO nao foi adicionado as fk
 
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            Messages.addGlobalError("Erro ao inserir placa de velocidade máxima");
+        }
     }
 
     @Override
     public VelocidadeMaxima read(Integer id) throws SQLException {
-        return null;
+        VelocidadeMaxima velocidadeMaxima = new VelocidadeMaxima();
+
+        try (PreparedStatement statement = connection.prepareStatement(BUSCA_VELOCIDADE_MAXIMA)) {
+            statement.setInt(1, id);
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    velocidadeMaxima.setIdVelocidadeMaxima(result.getInt("idVelocidadeMaxima"));
+                    velocidadeMaxima.setSituacao(result.getString("situacao"));
+                    velocidadeMaxima.setUf(result.getString("uf"));
+                    velocidadeMaxima.setAnoPnvSnc(result.getInt("anoPnvSnc"));
+                    velocidadeMaxima.setVeloVeicPesado(result.getFloat("veloVeicPesado"));
+                    velocidadeMaxima.setVeloVeicLeve(result.getFloat("veloVeicLeve"));
+                    velocidadeMaxima.setLatitude(result.getFloat("latitude"));
+                    velocidadeMaxima.setLongitude(result.getFloat("longitude"));
+                    velocidadeMaxima.setMunicipio(result.getString("municipio"));
+                    velocidadeMaxima.setKm(result.getFloat("km"));
+                } else {
+                    throw new SQLException("Erro ao visualizar: placa de velocidade máxima não pode ser encontrada.");
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVelocidadeMaximaDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+        }
+        return velocidadeMaxima;
     }
 
     @Override
     public void update(VelocidadeMaxima velocidadeMaxima) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_VELOCIDADE_MAXIMA)) {
+            statement.setInt(1, velocidadeMaxima.getIdVelocidadeMaxima());
+            statement.setString(2, velocidadeMaxima.getSituacao());
+            statement.setString(3, velocidadeMaxima.getUf());
+            statement.setInt(4, velocidadeMaxima.getAnoPnvSnc());
+            statement.setFloat(5, velocidadeMaxima.getVeloVeicPesado());
+            statement.setFloat(6, velocidadeMaxima.getVeloVeicLeve());
+            statement.setFloat(7, velocidadeMaxima.getLatitude());
+            statement.setFloat(8, velocidadeMaxima.getLongitude());
+            statement.setString(9, velocidadeMaxima.getMunicipio());
+            statement.setFloat(10, velocidadeMaxima.getKm());
 
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVelocidadeMaximaDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+        }
     }
 
     @Override
     public void delete(Integer id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_VELOCIDADE_MAXIMA)) {
+            statement.setInt(1, id);
 
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVelocidadeMaximaDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+        }
     }
 
     @Override
     public List<VelocidadeMaxima> all() throws SQLException {
-        return null;
+        List<VelocidadeMaxima> velocidadeMaximaList = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(BUSCA_TODAS_VELOCIDADES);
+             ResultSet result = statement.executeQuery()) {
+            while (result.next()) {
+                VelocidadeMaxima velocidadeMaxima = new VelocidadeMaxima();
+                velocidadeMaxima.setIdVelocidadeMaxima(result.getInt("idVelocidadeMaxima"));
+                velocidadeMaxima.setSituacao(result.getString("situacao"));
+                velocidadeMaxima.setUf(result.getString("uf"));
+                velocidadeMaxima.setAnoPnvSnc(result.getInt("anoPnvSnc"));
+                velocidadeMaxima.setVeloVeicPesado(result.getFloat("veloVeicPesado"));
+                velocidadeMaxima.setVeloVeicLeve(result.getFloat("veloVeicLeve"));
+                velocidadeMaxima.setLatitude(result.getFloat("latitude"));
+                velocidadeMaxima.setLongitude(result.getFloat("longitude"));
+                velocidadeMaxima.setMunicipio(result.getString("municipio"));
+                velocidadeMaxima.setKm(result.getFloat("km"));
+
+
+                velocidadeMaximaList.add(velocidadeMaxima);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVelocidadeMaximaDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+        }
+        return velocidadeMaximaList;
     }
 }

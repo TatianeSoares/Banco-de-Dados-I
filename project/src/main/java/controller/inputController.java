96f40d79f21deabd616a38de4f40e6cafc52a110
Tatiane <soaresferreiratatiane@gmail.com>
2022-11-06 22:31:05 -0300

    Arrumando arquivos do frontend

--- project/src/main/java/controller/inputController.java
+++ project/src/main/java/controller/inputController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.model.file.UploadedFile;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -8,11 +14,23 @@ import java.io.Serializable;
 @ViewScoped
 public class inputController implements Serializable {
 
+  @Getter @Setter
+  private UploadedFile file;
+  private String escolha;
+
   public void init(){
 
   }
 
   public void verificarArquivo(){
+
+  }
+
+  public void upload(){
+    if(file != null){
+      FacesMessage message = new FacesMessage("Sucesso", file.getFileName() + " upload está feito.");
+      FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 
   }
 }
